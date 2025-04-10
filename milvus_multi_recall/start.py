0fe0cc90daa2e700cc9e34f56ca73cadfb8357f0
刘晓 <2162408515@qq.com>
2025-04-10 16:01:02 +0800

    增加多路召回

--- milvus_multi_recall/start.py
+++ milvus_multi_recall/start.py
@@ -4,6 +4,11 @@ from ESPart.EsSearch import search_elasticsearch
 from milvusPart.milvusSearch import get_embedding
 from functioncall.FunctionTools import call_with_messages
 import gradio as gr
+import pandas as pd
+import cv2
+from dashscope import Generation
+from towhee import ops, pipe, DataCollection
+import numpy as np
 """
 多路召回：
 1. Milvus：使用Milvus进行向量搜索，获取最相似的文档。
@@ -17,19 +22,54 @@ import gradio as gr
 而对于ES部分，采用DFS QUERY THEN　ＦＥＴＣＨ方式，在查询之前先分散计算各分片的词频信息，然后再执行查询，这种方式能提供更加准确的结果。
 """
 
-def multi_recall(query_text, top_k=5):
+
+def read_image(image_ids):
+    df = pd.read_csv('reverse_image_search.csv')
+    id_img = df.set_index('id')['path'].to_dict()
+    imgs = []
+    decode = ops.image_decode.cv2('rgb')
+    for image_id in image_ids:
+        path = id_img[image_id]
+        imgs.append(decode(path))
+    return imgs
+
+
+search_pipeline = (
+    pipe.input('text')
+    .map('text', 'vec', ops.image_text_embedding.clip(model_name='model', modality='text'))
+    .map('vec', 'vec', lambda x: x / np.linalg.norm(x))
+    .map('vec', 'result', ops.ann_search.milvus_client(host='127.0.0.1', port='19530', collection_name='text_image_search', limit=5))
+    .map('result', 'image_ids', lambda x: [item[0] for item in x])
+    .output('image_ids')
+)
+
+def search(text):
+    df = pd.read_csv('reverse_image_search.csv')
+    id_img = df.set_index('id')['path'].to_dict()
+    imgs = []
+    image_ids = search_pipeline(text).to_list()[0][0]
+    return [id_img[image_id] for image_id in image_ids]
+
+
+def multi_recall(query_text):
     milvus_results,url = get_embedding(query_text)
     es_results = search_elasticsearch(query_text)
     function_calling_results = call_with_messages(query_text)
     all_results = milvus_results + es_results + function_calling_results
     all_results.sort(key=lambda x: x[1], reverse=True)
-    return all_results[:top_k],url
+    response = Generation.call(
+        api_key="",
+        model='qwen-plus',  
+        messages=messages,
+        temperature=0.6,
+    )
+    return response
 
 if __name__ == "__main__":
 
     query_text = ""
     
-    results,url = multi_recall(query_text, top_k=5)
+    results,url = multi_recall(query_text)
 
     messages = [
             {
@@ -60,7 +100,12 @@ if __name__ == "__main__":
         
         # 设置事件处理程序
         submit_button.click(fn=lambda x: multi_recall(x), inputs=input_text, outputs=output_text)
+        interface = gr.Interface(search, 
+                             gr.Textbox(lines=1),
+                             [gr.Image(type="filepath", label=None) for _ in range(5)]
+                            )
 
+    interface.launch(inline=True, share=True)
     # 启动 Gradio 应用
     demo.launch()    
 
